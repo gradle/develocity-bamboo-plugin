0a92d56a1b2caa287f4ea110c842a0e04883b57d
Nelson Osacky <nelson@osacky.com>
2023-07-27 09:00:13 +0200

    Update CCUD Maven extension version (#53)

--- src/test/java/com/gradle/enterprise/bamboo/MavenEmbeddedResourcesTest.java
+++ src/test/java/com/gradle/enterprise/bamboo/MavenEmbeddedResourcesTest.java
@@ -30,7 +30,7 @@ public class MavenEmbeddedResourcesTest {
     @ParameterizedTest
     @CsvSource({
             "GE_EXTENSION, d705a7adca376a6753cd9c1b096880c5",
-            "CCUD_EXTENSION, f4802380dd87ceaf4047874d0cfaeca9"
+            "CCUD_EXTENSION, d986e6377183b9283948abe8f89987a8"
     })
     void copiesEmbeddedExtension(MavenEmbeddedResources.Resource resource, String expectedChecksum) throws Exception {
         Path tmp = folder.resolve("extensions");
