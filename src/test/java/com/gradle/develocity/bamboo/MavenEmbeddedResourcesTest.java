7e4c63f113a907bfcb16dd610ff7226881b30860
Bot Githubaction <98587996+bot-githubaction@users.noreply.github.com>
2025-11-04 09:53:15 +0100

    Bump com.gradle:develocity-maven-extension from 2.2 to 2.2.1 (#305)

--- src/test/java/com/gradle/develocity/bamboo/MavenEmbeddedResourcesTest.java
+++ src/test/java/com/gradle/develocity/bamboo/MavenEmbeddedResourcesTest.java
@@ -30,7 +30,7 @@ public class MavenEmbeddedResourcesTest {
      */
     @ParameterizedTest
     @CsvSource({
-            "DEVELOCITY_EXTENSION, 9c35d1ddc04cc4be1e4b42dc4109b199",
+            "DEVELOCITY_EXTENSION, b6daf866db1e0f9843657a8f06f8beaf",
             "CCUD_EXTENSION, 0b46d6453cd8a76082b516d9f71a5bf8"
     })
     void copiesEmbeddedExtension(MavenEmbeddedResources.Resource resource, String expectedChecksum) throws Exception {
